25d88103932416326f3ef5ba9000d9a184357d07
fabriciovitorio <fabricio.vitorio@gmail.com>
2024-02-22 22:28:25 -0300

    secret_key e password

--- dundie/models/user.py
+++ dundie/models/user.py
@@ -2,6 +2,8 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
 
+from dundie.security import HashedPassword
+
 
 class User(SQLModel, table=True):
     """Represents the User Model"""
@@ -11,7 +13,8 @@ class User(SQLModel, table=True):
     username: str = Field(unique=True, nullable=False)
     avatar: Optional[str] = None
     bio: Optional[str] = None
-    password: str = Field(nullable=False)
+    #password: str = Field(nullable=False)
+    password: HashedPassword
     name: str = Field(nullable=False)
     dept: str = Field(nullable=False)
     currency: str = Field(nullable=False)
@@ -20,3 +23,10 @@ class User(SQLModel, table=True):
     def superuser(self):
         """"Users belonging to management dept are admins."""
         return self.dept == "management"
+
+def generate_username(name: str) -> str:
+    """Generates a slug username from a name
+    
+        "Fabricio Vitorio" -> "fabricio-vitorio"
+    """
+    return name.lower().replace(" ", "-")
