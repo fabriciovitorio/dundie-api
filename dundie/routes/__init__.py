d143d697b91889c334c789bd3057b5cb5d698224
fabriciovitorio <fabricio.vitorio@gmail.com>
2024-03-03 21:49:06 -0300

    autenticacao

--- dundie/routes/__init__.py
+++ dundie/routes/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from .user import router as user_router
+from .auth import router as auth_router
 
 main_router = APIRouter()
 # paramentro redirect_slashes, default true, faz funcionar passando ou nao o / no final da url, se quiser tirar para forçar colocar a / para funcionar precisa colocar false   
 
 main_router.include_router(user_router, prefix="/user", tags=["user"])
+main_router.include_router(auth_router, tags=["auth"])
--- dundie/routes/user.py
+++ dundie/routes/user.py
@@ -7,10 +7,12 @@ from sqlmodel import Session, select
 from dundie.db import ActiveSession
 from dundie.models.user import User, UserRequest, UserResponse
 
+from dundie.auth import AuthenticatedUser, SuperUser
+
 router = APIRouter()
 
 
-@router.get("/", response_model=List[UserResponse])
+@router.get("/", response_model=List[UserResponse], dependencies=[AuthenticatedUser])
 async def list_users(*, session: Session = ActiveSession):
     """List all users."""
     users = session.exec(select(User)).all()
@@ -28,8 +30,7 @@ async def get_user_by_username(
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
-
-@router.post("/", response_model=UserResponse, status_code=201)
+@router.post("/", response_model=UserResponse, status_code=201, dependencies=[SuperUser])
 async def create_user(*, session: Session = ActiveSession, user: UserRequest):
     """Creates new user"""
     db_user = User.from_orm(user)  # transform UserRequest in User
@@ -37,3 +38,5 @@ async def create_user(*, session: Session = ActiveSession, user: UserRequest):
     session.commit()
     session.refresh(db_user)
     return db_user
+   
+    
