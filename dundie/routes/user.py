dc8f40d218820f415d699718ced7bd038eb655f3
fabriciovitorio <fabricio.vitorio@gmail.com>
2024-03-04 21:38:32 -0300

    Errors e patch

--- dundie/routes/user.py
+++ dundie/routes/user.py
@@ -3,9 +3,10 @@ from typing import List
 from fastapi import APIRouter
 from fastapi.exceptions import HTTPException
 from sqlmodel import Session, select
+from sqlalchemy.exc import IntegrityError
 
 from dundie.db import ActiveSession
-from dundie.models.user import User, UserRequest, UserResponse
+from dundie.models.user import User, UserRequest, UserResponse, UserProfilePatchRequest
 
 from dundie.auth import AuthenticatedUser, SuperUser
 
@@ -33,10 +34,38 @@ async def get_user_by_username(
 @router.post("/", response_model=UserResponse, status_code=201, dependencies=[SuperUser])
 async def create_user(*, session: Session = ActiveSession, user: UserRequest):
     """Creates new user"""
+    if session.exec(select(User).where(User.username == user.username)).first():
+        raise HTTPException(status_code=409, detail="Username already taken")
+
     db_user = User.from_orm(user)  # transform UserRequest in User
     session.add(db_user)
-    session.commit()
+    try:
+        session.commit()
+    except IntegrityError:
+        raise HTTPException(status_code=500, detail="Database IntegrityError")
+
     session.refresh(db_user)
     return db_user
-   
     
+@router.patch("/{username}/", response_model=UserResponse)
+async def update_user(
+    *,
+    session: Session = ActiveSession,
+    patch_data: UserProfilePatchRequest,
+    current_user: User = AuthenticatedUser,
+    username: str
+):
+    user = session.exec(select(User).where(User.username == username)).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if user.id != current_user.id and not current_user.superuser:
+        raise HTTPException(status_code=403, detail="You can only update your own profile")
+
+    # Update
+    user.avatar = patch_data.avatar
+    user.bio = patch_data.bio
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
